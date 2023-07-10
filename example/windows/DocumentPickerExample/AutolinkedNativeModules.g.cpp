04fc54b1557a4173b862533a68940f3d0236c488
mackenzie <mackenzie@yourmoms>
2023-07-10 12:39:07 -0500

    chore(windows): link module

--- example/windows/DocumentPickerExample/AutolinkedNativeModules.g.cpp
+++ example/windows/DocumentPickerExample/AutolinkedNativeModules.g.cpp
@@ -3,12 +3,16 @@
 #include "pch.h"
 #include "AutolinkedNativeModules.g.h"
 
+// Includes from react-native-document-picker
+#include <winrt/ReactNativeDocumentPicker.h>
+
 namespace winrt::Microsoft::ReactNative
 {
 
 void RegisterAutolinkedNativeModulePackages(winrt::Windows::Foundation::Collections::IVector<winrt::Microsoft::ReactNative::IReactPackageProvider> const& packageProviders)
 { 
-    UNREFERENCED_PARAMETER(packageProviders);
+    // IReactPackageProviders from react-native-document-picker
+    packageProviders.Append(winrt::ReactNativeDocumentPicker::ReactPackageProvider());
 }
 
 }
