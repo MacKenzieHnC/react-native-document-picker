ca84d75835dcb0f282fe56a9dfbfe4084337ca2f
mackenzie <mackenzie@yourmoms>
2023-07-11 21:51:21 -0500

    feat: this should work but it doesn't

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { multiply } from 'react-native-document-picker';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      <Button
+        title={'press'}
+        onPress={() => {
+          multiply(3, 7).then(setResult);
+        }}
+      />
     </View>
   );
 }
--- windows/ReactNativeDocumentPicker/ReactNativeModule.h
+++ windows/ReactNativeDocumentPicker/ReactNativeModule.h
@@ -8,26 +8,25 @@ using namespace winrt::Microsoft::ReactNative;
 namespace winrt::ReactNativeDocumentPicker
 {
 
-REACT_MODULE(ReactNativeModule, L"ReactNativeDocumentPicker")
-struct ReactNativeModule
-{
-    // See https://microsoft.github.io/react-native-windows/docs/native-modules for details on writing native modules
+    REACT_MODULE(ReactNativeModule, L"DocumentPicker")
+    struct ReactNativeModule
+    {
+        // See https://microsoft.github.io/react-native-windows/docs/native-modules for details on writing native modules
 
-    REACT_INIT(Initialize)
-    void Initialize(ReactContext const &reactContext) noexcept
-    {
-        m_reactContext = reactContext;
-    }
-    
-    REACT_METHOD(sampleMethod)
-    void sampleMethod(std::string stringArgument, int numberArgument, std::function<void(std::string)> &&callback) noexcept
-    {
-        // TODO: Implement some actually useful functionality
-        callback("Received numberArgument: " + std::to_string(numberArgument) + " stringArgument: " + stringArgument);
-    }
+        REACT_INIT(Initialize)
+        void Initialize(ReactContext const &reactContext) noexcept
+        {
+            m_reactContext = reactContext;
+        }
+
+        REACT_METHOD(Multiply, L"multiply")
+        void Multiply(double a, double b, ReactPromise<double> promise) noexcept
+        {
+            promise.Resolve(a * b);
+        }
 
     private:
         ReactContext m_reactContext{nullptr};
-};
+    };
 
 } // namespace winrt::ReactNativeDocumentPicker
